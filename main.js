1af5f3ba35f2736cc9a604bfb7bdb877eb3cf822
eliseuSousa <eliseusousa759@gmail.com>
2022-08-14 19:06:02 -0300

    mostra os dados na tela

--- main.js
+++ main.js
@@ -5,6 +5,10 @@
   const inputMonth = document.querySelector('.month');
   const inputYear = document.querySelector('.year');
   const inputCvc = document.querySelector('.input__cvc');
+  const displayCardNumber = document.querySelectorAll('.number > span');
+  const displayName = document.querySelector('.nome');
+  const displayDateValidade = document.querySelector('.validade');
+  const displayCvc = document.querySelector('.card__back > span');
   const submitButton = document.querySelector('.submit');
 
   const validadeName = (event) => {
@@ -22,6 +26,7 @@
       submitButton.removeAttribute('disabled', 'disabled');
       inputName.classList.remove('error');
       inputName.nextElementSibling.classList.remove('error--active');
+      displayName.textContent = input.value;
     }
   }
 
@@ -31,7 +36,7 @@
 
     const testCardNumberTest = regex.test(input.value);
 
-    if(!testCardNumberTest) {
+    if(!testCardNumberTest && input.value != 16) {
       submitButton.setAttribute('disabled', 'disabled');
       inputCardNumber.classList.add('error');
       inputCardNumber.nextElementSibling.classList.add('error--active');
@@ -39,6 +44,10 @@
       submitButton.removeAttribute('disabled', 'disabled');
       inputCardNumber.classList.remove('error');
       inputCardNumber.nextElementSibling.classList.remove('error--active');
+      displayCardNumber[0].textContent = input.value.slice(0,4)
+      displayCardNumber[1].textContent = input.value.slice(4,8);
+      displayCardNumber[2].textContent = input.value.slice(8,12);
+      displayCardNumber[3].textContent = input.value.slice(12,16);
     }
   }
 
@@ -53,6 +62,12 @@
       submitButton.removeAttribute('disabled', 'disabled');
       inputMonth.classList.remove('error');
       inputYear.nextElementSibling.classList.remove('error--active');
+      
+      if (inputYear.value) {
+        displayDateValidade.textContent = `${inputMonth.value}/${inputYear.value}`;
+      } else {
+        displayDateValidade.textContent = `${inputMonth.value}/00`;
+      }
     }
   }
 
@@ -67,12 +82,17 @@
       submitButton.removeAttribute('disabled', 'disabled');
       inputYear.classList.remove('error');
       inputYear.nextElementSibling.classList.remove('error--active');
+
+      if (inputMonth.value) {
+        displayDateValidade.textContent = `${inputMonth.value}/${inputYear.value}`;
+      } else {
+        displayDateValidade.textContent = `00/${inputYear.value}`;
+      }
     }
   }
 
   const ValidaCvc = (event) => {
     const input = event.currentTarget.value;
-    
     if (input == '' || input.length != 3) {
       submitButton.setAttribute('disabled', 'disabled');
       inputCvc.classList.add('error');
@@ -81,6 +101,7 @@
       submitButton.removeAttribute('disabled', 'disabled');
       inputCvc.classList.remove('error');
       inputCvc.nextElementSibling.classList.remove('error--active');
+      displayCvc.textContent = input;
     }
   }
 
